087ea61e3ebdab1e7e9489e3abac34c93f85001d
Nick5928 <120755652+Nick5928@users.noreply.github.com>
2024-04-15 14:54:52 -0400

    Update recipe.js

--- static/js/recipe.js
+++ static/js/recipe.js
@@ -21,7 +21,39 @@ $(document).ready(function(){
 
 
 
+    $(document).on("click", ".recipe_add", function() {
 
+        var row = $(this).closest("tr")
+
+        var link = row.find(".link_cell").find("a").attr("href");
+        var recipe = row.find(".link_cell").text().trim();
+        var carbs = row.find(".carbs_cell").text().trim();
+        var fat = row.find(".fat_cell").text().trim();
+        var protein = row.find(".protein_cell").text().trim();
+        var calories = row.find(".calories_cell").text().trim();
+
+
+        meal_data = {
+            "recipe_name": recipe,
+            "recipe_link": link,
+            "calories": calories,
+            "fat": fat,
+            "protein" : protein,
+            "carbohydrates": carbs
+        }
+
+        console.log(meal_data)
+
+        $.ajax({
+            url: '/api/add_meal',
+            type : 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(meal_data),
+            success: function(response) {
+                console.log(response);
+            }
+        })
+    });
 
 
 
